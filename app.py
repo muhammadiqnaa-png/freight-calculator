c34eba55931904b4b2d397b7c381e559256115a2
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-09-15 22:19:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.title("🚢 Freight Calculator Batubara")
 # ==============================
 st.sidebar.header("⚙️ Parameter Default (Bisa diubah)")
 
-speed_kosong = st.sidebar.number_input("Speed Kosong (knot)", value=4)
-speed_isi = st.sidebar.number_input("Speed Isi (knot)", value=3)
+speed_kosong = st.sidebar.number_input("Speed Kosong (knot)", value=4.0)
+speed_isi = st.sidebar.number_input("Speed Isi (knot)", value=3.0)
 consumption = st.sidebar.number_input("Consumption (liter/jam)", value=0)
 harga_bunker = st.sidebar.number_input("Harga Bunker (Rp/liter)", value=0)
 
