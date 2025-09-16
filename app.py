8c9fe9d56a9e74e450244c77812cbd2418ecad44
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-09-17 02:26:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,6 @@ def generate_pdf(input_data, results, profit_df):
     table_input = Table(input_data, colWidths=[200, 200])
     table_input.setStyle(TableStyle([
         ("GRID", (0,0), (-1,-1), 0.5, colors.grey),
-        ("BACKGROUND", (0,0), (-1,0), colors.whitesmoke),
         ("FONTSIZE", (0,0), (-1,-1), 9)
     ]))
     elements.append(table_input)
@@ -174,7 +173,6 @@ def generate_pdf(input_data, results, profit_df):
     table_results = Table(results, colWidths=[200, 200])
     table_results.setStyle(TableStyle([
         ("GRID", (0,0), (-1,-1), 0.5, colors.grey),
-        ("BACKGROUND", (0,0), (-1,0), colors.whitesmoke),
         ("FONTSIZE", (0,0), (-1,-1), 9)
     ]))
     elements.append(table_results)
