0e04e095ec58b03af4a134cc742574165a00784c
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-11-01 22:34:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import math
 import pandas as pd
 from io import BytesIO
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Image
 from reportlab.lib import colors
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from datetime import datetime
 import requests
 
