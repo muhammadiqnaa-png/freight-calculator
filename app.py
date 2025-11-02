777a4c5ecea22588be34760a81702ada1e89c0b7
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-11-02 12:57:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -367,8 +367,8 @@ if st.button("Calculate Freight 💸"):
             doc = SimpleDocTemplate(
                 buffer,
                 pagesize=A4,
-                rightMargin=25,
-                leftMargin=25,
+                rightMargin=5,
+                leftMargin=5,
                 topMargin=0,
                 bottomMargin=0
             )
