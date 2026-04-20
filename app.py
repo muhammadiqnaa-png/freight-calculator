19441166ca300747031ae616507df555047188d7
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-20 22:09:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ if not st.session_state.logged_in:
     # =========================
     # PAGE: REGISTER
     # =========================
-    if st.session_state.page == "Create Account":
+    if st.session_state.page == "Register":
 
         st.markdown("<h2 style='text-align:center;'>Create Account</h2>", unsafe_allow_html=True)
 
