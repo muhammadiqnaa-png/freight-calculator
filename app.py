1ab0ff0bde4e736bf119577a3e7f8a8e5204006c
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-22 10:11:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ if not st.session_state.intro_done:
         font-size:65px;
         font-weight:1000;
         margin-bottom:5px;
-        line-height:0.5;
+        line-height:0;
     ">
         🚢 Welcome 🚢
     </h1>
@@ -394,7 +394,7 @@ if not st.session_state.intro_done:
         font-size:65px;
         font-weight:1000;
         margin-bottom:5px;
-        line-height:0.5;
+        line-height:0;
     ">
         Freight Calculator
     </h1>
