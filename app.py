0fdb261871f8f7e69bbd103e52ac2d34fdd35808
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-23 22:53:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1488,7 +1488,9 @@ if calculate:
         (sailing POL→POD {pol_pod_day:.1f} Days - POD→POL {pod_pol_day:.1f} Days)
         </span><br>
         • Freight Cost: <b style="color:#0f172a;">Rp {freight_cost_mt:,.0f}</b>
-        {pricing_block}
+
+        • Recommended Freight: 
+        <b style="color:#f97316;">Rp {ideal_freight:,.0f}</b>
         
         </div>
         """, unsafe_allow_html=True)
