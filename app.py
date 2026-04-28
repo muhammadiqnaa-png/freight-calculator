08aaedf5bba982d195bb9ece2b1e2a3bf17f904d
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-28 17:40:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 from distance import *
+from auth import *
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Image
 from reportlab.lib import colors
@@ -256,20 +257,6 @@ st.markdown("""
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <meta name="apple-mobile-web-app-title" content="FreightCalc">
 """, unsafe_allow_html=True)
-
-
-# ====== FIREBASE AUTH ======
-FIREBASE_API_KEY = st.secrets["FIREBASE_API_KEY"]
-AUTH_URL = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_API_KEY}"
-REGISTER_URL = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={FIREBASE_API_KEY}"
-
-def login_user(email, password):
-    res = requests.post(AUTH_URL, json={"email": email, "password": password, "returnSecureToken": True})
-    return res.ok, res.json()
-
-def register_user(email, password):
-    res = requests.post(REGISTER_URL, json={"email": email, "password": password, "returnSecureToken": True})
-    return res.ok, res.json()
 
 
 # ✅ AUTO LOGIN DARI COOKIE (WAJIB DI ATAS)
