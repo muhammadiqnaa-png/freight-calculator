d29839dd9f081286c17d0151ee0a4464d6093d31
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-28 17:35:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from io import BytesIO
+from distance import *
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Image
 from reportlab.lib import colors
@@ -27,60 +28,6 @@ if "hide_intro" not in st.session_state:
 # ambil dari cookies (persist)
 if cookies.get("hide_intro") == "true":
     st.session_state.hide_intro = True
-
-DATA_FILE = "distance_data.json"
-
-def find_distance(pol, pod):
-    data = load_distances()
-
-    pol = (pol or "").strip().upper()
-    pod = (pod or "").strip().upper()
-
-    for route, distance in data.items():
-        try:
-            p, d = route.split(" - ")
-
-            p = p.strip().upper()
-            d = d.strip().upper()
-
-            # ✅ normal match
-            if p == pol and d == pod:
-                return distance
-
-            # 🔥 reverse match (INI KUNCI FIX LU)
-            if p == pod and d == pol:
-                return distance
-
-        except:
-            continue
-
-    return 0
-
-def load_distances():
-    if not os.path.exists(DATA_FILE):
-        return {}
-
-    with open(DATA_FILE, "r") as f:
-        return json.load(f)
-
-
-def save_distances(data):
-    with open(DATA_FILE, "w") as f:
-        json.dump(data, f, indent=2)
-
-def get_all_ports():
-    data = load_distances()
-    ports = set()
-
-    for route in data.keys():
-        try:
-            pol, pod = route.split(" - ")
-            ports.add(pol.upper())
-            ports.add(pod.upper())
-        except:
-            continue
-
-    return sorted(list(ports))
 
 
 # ==========================================================
