997a6503b9f81b56f2848169155f3d4ab3d8bbe9
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-29 19:07:19 +0700

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -3,16 +3,18 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # =========================
-# INIT FIREBASE (SAFE)
+# INIT FIREBASE
 # =========================
 
 if not firebase_admin._apps:
 
+    private_key = st.secrets["firebase"]["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n")
+
     cred = credentials.Certificate({
         "type": "service_account",
         "project_id": st.secrets["firebase"]["FIREBASE_PROJECT_ID"],
-        "private_key": st.secrets["firebase"]["FIREBASE_PRIVATE_KEY"].
         "client_email": st.secrets["firebase"]["FIREBASE_CLIENT_EMAIL"],
+        "private_key": private_key,
         "token_uri": "https://oauth2.googleapis.com/token"
     })
 
@@ -20,8 +22,8 @@ if not firebase_admin._apps:
         "databaseURL": st.secrets["firebase"]["FIREBASE_DB_URL"]
     })
 
+# =========================
+# DB REFERENCE
+# =========================
 
-# =========================
-# REFERENCE ROOT DB
-# =========================
 ref = db.reference("/")
