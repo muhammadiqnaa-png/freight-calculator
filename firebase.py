a1f0ac6608a19bee3460d08325d3ee83c441b5ec
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-29 19:06:20 +0700

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -2,22 +2,26 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 
-# ===== BUILD CREDENTIAL MANUAL DARI SECRETS =====
-cred_dict = {
-    "type": "service_account",
-    "project_id": st.secrets["FIREBASE_PROJECT_ID"],
-    "private_key_id": "dummy",
-    "private_key": st.secrets["FIREBASE_PRIVATE_KEY"],
-    "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
-    "token_uri": "https://oauth2.googleapis.com/token"
-}
+# =========================
+# INIT FIREBASE (SAFE)
+# =========================
 
-cred = credentials.Certificate(cred_dict)
-
-# ===== INIT FIREBASE =====
 if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred, {
-        "databaseURL": st.secrets["FIREBASE_DB_URL"]
+
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": st.secrets["firebase"]["FIREBASE_PROJECT_ID"],
+        "private_key": st.secrets["firebase"]["FIREBASE_PRIVATE_KEY"].
+        "client_email": st.secrets["firebase"]["FIREBASE_CLIENT_EMAIL"],
+        "token_uri": "https://oauth2.googleapis.com/token"
     })
 
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": st.secrets["firebase"]["FIREBASE_DB_URL"]
+    })
+
+
+# =========================
+# REFERENCE ROOT DB
+# =========================
 ref = db.reference("/")
