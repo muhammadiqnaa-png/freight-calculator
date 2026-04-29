f21b7f75f90a72e51055f4b0c5aeb47a49500310
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-29 18:35:13 +0700

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import json
 
-firebase_json = json.loads(st.secrets["FIREBASE_SERVICE_ACCOUNT"])
+firebase = st.secrets["firebase"]
 
-cred = credentials.Certificate(firebase_json)
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": firebase["FIREBASE_PROJECT_ID"],
+    "private_key": firebase["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+    "client_email": firebase["FIREBASE_CLIENT_EMAIL"],
+})
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
-        "databaseURL": st.secrets["firebase"]["FIREBASE_DB_URL"]
+        "databaseURL": firebase["FIREBASE_DB_URL"]
     })
 
 ref = db.reference("/")
