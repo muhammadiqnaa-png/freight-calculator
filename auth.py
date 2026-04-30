672b004de3862348f79dd48d385c77fd90e638da
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-30 18:35:19 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -20,17 +20,13 @@ FIREBASE_PRIVATE_KEY = firebase.get("FIREBASE_PRIVATE_KEY")
 # 🔥 INIT FIREBASE
 # =========================
 if not firebase_admin._apps:
+
     cred = credentials.Certificate({
         "type": "service_account",
         "project_id": FIREBASE_PROJECT_ID,
-        "private_key_id": "dummy",
         "private_key": FIREBASE_PRIVATE_KEY.replace("\\n", "\n"),
         "client_email": FIREBASE_CLIENT_EMAIL,
-        "client_id": "dummy",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "dummy"
+        "token_uri": "https://oauth2.googleapis.com/token"
     })
 
     firebase_admin.initialize_app(cred, {
