d5bc1ae6a68e403d2bbfd2b46ef5711f3942aaeb
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-05-01 01:02:24 +0700

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,28 +1,14 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import streamlit as st
 
 # =========================
 # LOAD SECRETS
 # =========================
 firebase_dict = dict(st.secrets["firebase"])
 
-# =========================
-# 🔥 FORCE FIX PRIVATE KEY (IMPORTANT)
-# =========================
-key = firebase_dict["private_key"]
-
-# double safety fix (ini penting banget)
-key = key.replace("\\n", "\n")
-key = key.replace("\n\n", "\n").strip()
-
-firebase_dict["private_key"] = key
-
-# =========================
-# DEBUG CHECK (biar pasti)
-# =========================
-if "BEGIN PRIVATE KEY" not in firebase_dict["private_key"]:
-    raise ValueError("Private key rusak / tidak valid PEM format")
+# FIX PRIVATE KEY FORMAT
+firebase_dict["private_key"] = firebase_dict["private_key"].replace("\\n", "\n")
 
 # =========================
 # INIT FIREBASE
@@ -31,9 +17,12 @@ if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_dict)
 
     firebase_admin.initialize_app(cred, {
-        "databaseURL": firebase_dict.get("databaseURL")
+        "databaseURL": firebase_dict["databaseURL"]
     })
 
 
+# =========================
+# GET REFERENCE FUNCTION
+# =========================
 def get_ref(path):
     return db.reference(path)
