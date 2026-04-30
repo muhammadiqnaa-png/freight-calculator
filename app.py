dec9d5c5149f64a1f5f30e213f521b8ee41f08c0
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-30 18:05:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,6 @@ def save_pdf_history(pol, pod, email, file_name, pdf_bytes):
 def track_login(email):
 
     ref.child("user_activity").child(email.replace(".", "_")).update({
-        now = datetime.now(wib)
         last_login = now.strftime("%Y-%m-%d %H:%M")
     })
 
