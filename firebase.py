4f3bae7e1c984e6180c26e6e86b98dd5c5194476
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-05-01 00:41:54 +0700

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,18 +1,35 @@
 import firebase_admin
 from firebase_admin import credentials, db
 import streamlit as st
+import json
 
+# =========================
+# LOAD SECRETS
+# =========================
 firebase_dict = dict(st.secrets["firebase"])
 
+# =========================
+# FIX PRIVATE KEY
+# =========================
 firebase_dict["private_key"] = firebase_dict["private_key"].replace("\\n", "\n").strip()
 
+# =========================
+# 🔥 CONVERT KE JSON CLEAN (FIX STABILITY ISSUE)
+# =========================
+firebase_json = json.loads(json.dumps(firebase_dict))
+
+# =========================
+# INIT FIREBASE
+# =========================
 if not firebase_admin._apps:
-    cred = credentials.Certificate(firebase_dict)
+    cred = credentials.Certificate(firebase_json)
 
     firebase_admin.initialize_app(cred, {
         "databaseURL": firebase_dict.get("databaseURL")
     })
 
-
+# =========================
+# REF FUNCTION
+# =========================
 def get_ref(path):
     return db.reference(path)
