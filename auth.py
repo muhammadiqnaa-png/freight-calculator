ff01bd903dd177bc141a42a14a77b791d7fd7c9f
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-05-01 01:55:10 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import requests
 
+# ====== FIREBASE AUTH ======
+FIREBASE_API_KEY = st.secrets["FIREBASE_API_KEY"]
+AUTH_URL = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_API_KEY}"
+REGISTER_URL = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={FIREBASE_API_KEY}"
+
+def login_user(email, password):
+    res = requests.post(AUTH_URL, json={"email": email, "password": password, "returnSecureToken": True})
+    return res.ok, res.json()
+
+def register_user(email, password):
+    res = requests.post(REGISTER_URL, json={"email": email, "password": password, "returnSecureToken": True})
+    return res.ok, res.json()
+
+
 # =========================
 # 🔐 REGISTER USER
 # =========================
